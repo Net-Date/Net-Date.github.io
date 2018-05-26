f14f91e7b048e79d1cedb35532cab7c750271e4c
Bradctuwi <32627804+Bradctuwi@users.noreply.github.com>
2018-05-26 00:02:32 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
+function dropHandler(ev) {
+	//from mozilla firefox developer website
+  console.log('File(s) dropped');
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (ev.dataTransfer.items[i].kind === 'file') {
+        var file = ev.dataTransfer.items[i].getAsFile();
+        console.log('... file[' + i + '].name = ' + file.name);
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+    }
+  } 
+  
+  // Pass event to removeDragData for cleanup
+  removeDragData(ev)
+}
+function dragOverHandler(ev) {
+  console.log('File(s) in drop zone'); 
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+}
+(function($) {
+	"use strict"
+	
+	// Preloader
+	$(window).on('load', function() {
+		$("#preloader").delay(600).fadeOut();
+	});
+
+	// Mobile Toggle Button
+	$('.navbar-toggle').on('click',function(){
+		$('#header').toggleClass('nav-collapse')
+	});
+	
+})(jQuery);
+
